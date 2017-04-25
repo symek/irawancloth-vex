6a875c22936f4dcb91286fcf1a69672c80f4a96d
symek <szymon.kapeniak@gmail.com>
2017-04-25 11:53:07 +0200

    Moving from variadic to specialized signature for variable output.

--- src/VEX_thunderloom.cpp
+++ src/VEX_thunderloom.cpp
@@ -128,13 +128,13 @@ static void irawan_open(int argc, void *argv[], void *data)
 
 }
 
-static void irawan_sample(int argc, VEX_VexOpArg argv[], void *data)
+static void irawan_sample(int argc, void *argv[], void *data)
 {
-          VEXvec3  *result    = (VEXvec3*)        argv[0].myArg;
-    const VEXint   *handle    = (const VEXint*)   argv[1].myArg;
-    const VEXvec3  *wi        = (const VEXvec3*)  argv[2].myArg;
-    const VEXvec3  *wo        = (const VEXvec3*)  argv[3].myArg;
-    const VEXvec3  *uvw       = (const VEXvec3*)  argv[4].myArg;
+          VEXvec3 *result =       (VEXvec3*) argv[0];
+    const VEXint  *handle = (const VEXint* ) argv[1];
+    const VEXvec3 *wi     = (const VEXvec3*) argv[2];
+    const VEXvec3 *wo     = (const VEXvec3*) argv[3];
+    const VEXvec3 *uvw    = (const VEXvec3*) argv[4];
     
 
     std::map<int, IrawanInstance>::iterator it;
@@ -144,41 +144,45 @@ static void irawan_sample(int argc, VEX_VexOpArg argv[], void *data)
 
     tlIntersectionData intersection;
     fill_intersect(intersection, wi, wo, uvw);
-    tlPatternData pattern = tl_get_pattern_data(intersection, shader->parms);
-     // std::cout << "variadics num " << argc << '\n';
 
-    for (int i=5; i < argc; i=i+2) 
-    {
-       if(argv[i].myType == VEX_TYPE_STRING) {
-        
-        const char* variadic =  (const char*)argv[i].myArg;
-
-        if (std::strcmp(variadic, "diffuse") == 0) {
-          
-            tlColor col = tl_eval_diffuse(intersection, pattern, shader->parms);
-            VEXvec3 *diff = (VEXvec3*)argv[i+1].myArg;
-            diff->x() = col.r;
-            diff->y() = col.g;
-            diff->z() = col.b;
-            //i++;
-        } 
-        else if  (std::strcmp(variadic, "specular") == 0) {
-            
-             float spec = tl_eval_specular(intersection, pattern, shader->parms);
-             VEXfloat *rspec = (VEXfloat*)argv[i+1].myArg;
-             rspec[0] = spec;
-             //i++;
-        }
-
-       }
-
-    }
-
-   
     tlColor col = tl_shade(intersection, shader->parms);
     result->x() = (float)col.r;
     result->y() = (float)col.g;
     result->z() = (float)col.b;
+
+}
+
+static void irawan_sample(int argc, void *argv[], void *data)
+{
+
+    const VEXint   *handle = (const VEXint* ) argv[0];
+    const VEXvec3  *wi     = (const VEXvec3*) argv[1];
+    const VEXvec3  *wo     = (const VEXvec3*) argv[2];
+    const VEXvec3  *uvw    = (const VEXvec3*) argv[3];
+          VEXvec3  *diff   = (const VEXvec3*) argv[4];
+          VEXfloat *spec   = (const VEXfloat*)argv[5];
+          VEXvec3  *N      = (const VEXvec3*) argv[6];
+          VEXfloat *hit    = (const VEXfloat*)argv[7];
+
+    std::map<int, IrawanInstance>::iterator it;
+    it = IrawanStore.find(*handle);
+    UT_ASSERT(it != IrawanStore.end());
+    IrawanInstance *shader = static_cast<IrawanInstance*>(&it->second);
+
+    tlIntersectionData intersection;
+    fill_intersect(intersection, wi, wo, uvw);
+
+    tlPatternData pattern = tl_get_pattern_data(intersection, shader->parms);
+
+    tlColor col = tl_eval_diffuse(intersection, pattern, shader->parms);
+    diff->x() = col.r; diff->y() = col.g; diff->z() = col.b;
+    spec[0]   = tl_eval_specular(intersection, pattern, shader->parms);
+       
+
+    }
+
+}
+   
 
 }
 
@@ -197,15 +201,23 @@ newVEXOp(void *)
             irawan_open, 
             VEX_SURFACE_CONTEXT, 
             NULL, /*irawan_open_init,*/
-            irawan_open_cleanup, 
+            NULL, /*irawan_open_cleanup, */
             VEX_OPTIMIZE_2);
 
-    new VEX_VexOp("irawan_sample@&VIVVV+", /*vector color = irawan_sample(handle, wo, wi, uv)*/ 
+    new VEX_VexOp("irawan_sample@&VIVVV", /*vector color = irawan_sample(handle, wo, wi, uv)*/ 
             irawan_sample, 
             VEX_SURFACE_CONTEXT, 
             NULL, /*irwan_open_init,*/
             NULL, /*irwan_open_cleanup,*/ 
-            VEX_OPTIMIZE_1);
+            VEX_OPTIMIZE_2);
+
+      new VEX_VexOp("irawan_sample@IVVV&V&F&V&F", /*void irawan_sample(handle, wo, wi, uv, &diff, &spec, &N, &hit)*/ 
+            irawan_sample, 
+            VEX_SURFACE_CONTEXT, 
+            NULL, /*irwan_open_init,*/
+            NULL, /*irwan_open_cleanup,*/ 
+            VEX_OPTIMIZE_2,
+            false);
 
 
 }
